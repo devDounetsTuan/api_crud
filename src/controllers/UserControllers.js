9f8226f0ce2427cdff7b99d40d2c42ae265a5989
devDounetsTuan <45481311+devDounetsTuan@users.noreply.github.com>
2019-04-05 15:05:27 +0700

    update login logout with session

--- src/controllers/UserControllers.js
+++ src/controllers/UserControllers.js
@@ -5,8 +5,7 @@ exports.register = function (req, res, next) {
     User.findOne({
         email: req.body.email       
     }, (err, user) => {
-        if (user == null) {
-           
+        if (user == null) {          
             bcrypt.hash(req.body.password, 10, (err, hash) => {
                 if (err) {
                     return next();
@@ -25,7 +24,6 @@ exports.register = function (req, res, next) {
                     })
                 })
             })
-        // console.log(user);
         } else {
             res.json({
                 err: 'Email has been used'
@@ -40,18 +38,36 @@ exports.login = function(req, res){
         if(err) {
             return res.json({err})
         }else if (!user){
-            return res.json({err: 'Username and Password are incorrect'})
+            return res.json({err: 'Email are incorrect'})
         }
-        bcrypt.compare(req.body.password, user.password, (err, result) => {
-            if(result === true){
+       // console.log(user);
+        bcrypt.compare(req.body.password, user.password, (err, result) => {   
+            if(result){
+                //console.log(req);
+                //req.session = {};
                 req.session.user = user
+                //console.log(req.mySession.user);
                 res.json({
                     user: user,
                     "login": "success"
                 })
             }else{
-                return res.json({err: 'Username and Password are incorrect'})
+                return res.json({err: 'Password are incorrect'})
             }
         })
     })
+}
+
+exports.logout = function(req, res){
+    console.log("tuan "+req.session)
+    if (req.session) {
+        // delete session object
+        req.session.destroy(function(err) {
+            if(err) {
+                return res.json({err});
+            } else {
+                return res.json({'logout': "Success"});
+            }
+        });
+    }
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {register,login} = require('../controllers/UserControllers')
+const {register,login,logout} = require('../controllers/UserControllers')
 const {UserValidator} = require('../validators/validator')
 
 router.post('/register', register)
@@ -9,10 +9,22 @@ router.post('/register', register)
 router.post('/login', requiresLogout, login)
 
 function requiresLogout(req, res, next){
+  //  console.log(req.session.user)
     if (req.session && req.session.user) {
         return res.json({err: 'You must be Logout in to Login continue'});        
     } else {
         return next();
     }
 }
+
+function requiresLogin(req, res, next) {
+ console.log(req.session.user);
+    if (req.session && req.session.user) {
+        return next();
+    } else {
+        return res.json({err: 'You must be logged in to view this page.'});
+    }
+}
+
+router.get('/logout', requiresLogin, logout)
 module.exports = router;
